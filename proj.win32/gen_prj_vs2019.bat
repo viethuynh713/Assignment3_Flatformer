b1c7530a2267d07f054416c0f451b08b7605bcca
giabao04082001 <giabao04082001@gmail.com>
2022-10-21 00:22:01 +0700

    ! edit script

--- proj.win32/gen_prj_vs2019.bat
+++ proj.win32/gen_prj_vs2019.bat
@@ -1,2 +1,5 @@
 @echo off
-call cmake .. -G "Visual Studio 16 2019" -AWin32
+call mkdir vs2019
+call cd vs2019
+call cmake ../.. -G "Visual Studio 16 2019" -AWin32
+call cd ..
--- proj.win32/gen_prj_vs2022.bat
+++ proj.win32/gen_prj_vs2022.bat
@@ -1,2 +1,5 @@
 @echo off
-call cmake .. -G "Visual Studio 17 2022" -AWin32
+call mkdir vs2022
+call cd vs2022
+call cmake ../.. -G "Visual Studio 17 2022" -AWin32
+call cd ..
